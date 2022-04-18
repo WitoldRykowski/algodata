eafdb9bbddf5c95f76348812e3e79b5eca78c054
Witold Rykowski <witold.rykowski@machineportal.com>
2022-04-18 19:51:55 +0200

    try docs

--- src/DataStructures/LinkedList/LinkedList.ts
+++ src/DataStructures/LinkedList/LinkedList.ts
@@ -4,6 +4,19 @@ import LinkedListHelper from './LinkedListHelper'
 import { quickSort } from '../../Algorithms/QuickSort'
 
 export class LinkedList {
+  /**
+   * Returns the average of two numbers.
+   *
+   * @remarks
+   * This method is part of the nothing.
+   *
+   * @param x - The first input number
+   * @param y - The second input number
+   * @returns The arithmetic mean of `x` and `y`
+   *
+   * @beta
+   */
+
   private header: Node = null
   private last: Node = null
   private currentLength = 0
